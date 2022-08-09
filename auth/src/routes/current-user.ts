7418d3e14ef7dde1faf6a203637f6c8fcdac0d07
Blue-Bug <kjj9987@gmail.com>
2022-08-10 00:54:16 +0900

    Feat: Common 모듈 적용 - auth 프로젝트에 Common 모듈 적용 완료

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,6 +1,5 @@
 import express from 'express';
-import { currentUser } from '../middlewares/current-user';
-import { requireAuth } from '../middlewares/require-auth';
+import { currentUser } from '@special-lunch/common'
 
 const router = express.Router();
 
